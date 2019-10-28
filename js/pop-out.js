fb24b9ae3a952644c683a7654171f75d197ab9aa
Raghav <raghavkaushalfam@gmail.com>
2019-10-29 00:03:21 +0530

    content added

---
+++ js/pop-out.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $("#popUpMain").css("display", "block");
+  }, 5000);
+});
+$(".submitId"),
+  click(function() {
+    $("#popUpMain").css("display", "none");
+  });
